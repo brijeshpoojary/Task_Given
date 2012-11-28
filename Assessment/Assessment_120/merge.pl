36df717c3e5a4a1c53ef844a52b0e9a0bc83ea9b
BRIJESH <brijesh@ubuntu.ubuntu-domain>
2012-11-28 15:00:27 +0530

    updated

---
+++ Assessment/Assessment_120/merge.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+my $file = $ARGV[0] || 'q.lst';
+
+open(INP, $file) || die "Cannot open $file: $!";
+
+my (%stud, %assess);
+while (<INP>) {
+    chomp;
+    my @d = split(/\|/);
+    $stud{$d[6]}{data} = [ @d[0..11] ];
+    $stud{$d[6]}{$d[12]} = $d[13];
+    $assess{$d[12]} = 1;
+}
+close(INP);
+
+# print Dumper(\%stud);
+
+print 'DIST|BLK|CLUST|SCODE|SNAME|MOI|STUID|CSEX|CLANG|CLASS|ASSMNTNAME|PROGNAME|';
+foreach my $a (sort keys %assess) {
+    print join('|',$a), '|';
+}
+print "\n";
+
+foreach my $sid (keys %stud) {
+    print join('|', @{$stud{$sid}{data}}), '|';
+    foreach my $a (sort keys %assess) {
+        print join('|',$stud{$sid}{$a}), '|';
+    }
+    print "\n";
+}
+
---
+++ Semis_Data/filter/upload.py
@@ -0,0 +1,16 @@
+import psycopg2
+import csv
+import re
+con=psycopg2.connect(dbname='sslc',user='postgres',password='hello',host='localhost')
+cur=con.cursor()
+
+def upload():
+	fp=csv.reader(open('/home/brijesh/sslc1.csv','r'),delimiter='|',quotechar='\'')
+	i=0
+	for row in fp:
+		if(i!=0):	
+			cur.execute("insert into tb_sslc_data values('" + row[0] + "','" + row[1].replace("'","\\'") + "')")
+		i=i+1	
+	con.commit()
+upload()
+
